4e8c80102346025ed93bd48ad257858004a410e2
dewski <me@garrettbjerkhoel.com>
2012-01-24 20:25:34 -0800

    Update routes into the Tolk isolated namespace

--- lib/tolk/engine.rb
+++ lib/tolk/engine.rb
@@ -1,7 +1,15 @@
-require 'rails'
+require 'rails/engine'
+require 'ya2yaml'
+require 'will_paginate'
+
+require 'tolk/import'
+require 'tolk/sync'
 
 module Tolk
   class Engine < Rails::Engine
+    isolate_namespace Tolk
+
+    # Used as default namespace for routes
     engine_name :tolk
   end
 end
