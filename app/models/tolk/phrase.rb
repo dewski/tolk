5c2b32acdb6905b62578acbc4e6866ee8aaca01c
dewski <me@garrettbjerkhoel.com>
2012-01-24 20:49:14 -0800

    Update to Rails 3 ActiveModel

--- app/models/tolk/phrase.rb
+++ app/models/tolk/phrase.rb
@@ -1,17 +1,17 @@
 module Tolk
   class Phrase < ActiveRecord::Base
-    validates_uniqueness_of :key
+    validates :key, :uniqueness => true
 
     cattr_accessor :per_page
     self.per_page = 30
 
     has_many :translations, :class_name => 'Tolk::Translation', :dependent => :destroy do
       def primary
-        to_a.detect {|t| t.locale_id == Tolk::Locale.primary_locale.id}
+        to_a.detect { |t| t.locale_id == Tolk::Locale.primary_locale.id }
       end
 
       def for(locale)
-        to_a.detect {|t| t.locale_id == locale.id}
+        to_a.detect { |t| t.locale_id == locale.id }
       end
     end
 
