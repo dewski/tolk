3c1f252f0b9e8d42045c77ffa9740eda015ab238
dewski <me@garrettbjerkhoel.com>
2012-01-27 07:51:03 -0800

    Keep track of the original locale and add some configuration

--- lib/tolk.rb
+++ lib/tolk.rb
@@ -4,12 +4,24 @@ require 'tolk/keys'
 module Tolk
   class LocaleNotFound < StandardError; end
   
-  class << self
-    cattr_accessor :namespace
-    self.namespace = ENV['TOLK_NAMESPACE'] || 'tolk'
-    
-    cattr_accessor :locale
-    self.locale = ::I18n.default_locale.to_s
+  mattr_accessor :namespace
+  self.namespace = ENV['TOLK_NAMESPACE'] || 'tolk'
+  
+  mattr_accessor :default_locale
+  self.default_locale = ::I18n.default_locale.to_s
+  
+  mattr_accessor :locale
+  self.locale = ::I18n.default_locale.to_s
+  
+  mattr_accessor :hit_check
+  self.hit_check = true
+  
+  def self.hit_check?
+    !!hit_check
+  end
+
+  def self.setup
+    yield self
   end
   
   extend Keys
